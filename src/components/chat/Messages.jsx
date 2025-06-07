244d31f0d39dd28151ba0d5d81df82bf4835f0d2
Ronal <ronal.sanx21@gmail.com>
2025-06-07 17:01:48 -0500

    Cambios

--- src/components/chat/Messages.jsx
+++ src/components/chat/Messages.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeaderMessage from './HeaderMessage'
 import InputMessage from './InputMessage'
+import MessagesChat from './MessagesChat'
 
 function Messages() {
   return (
    <>
-    <div className="bg-gray-200 flex flex-col h-auto md:h-screen justify-between p-5">
+    <div className="grid grid-rows-[auto_3fr_auto] h-screen">
       <HeaderMessage/>
+      <MessagesChat/>
       <InputMessage/>
     </div>
    </>
