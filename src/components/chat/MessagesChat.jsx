99aa117659443c605a2b60525ee87c2662be19d7
Ronal <ronal.sanx21@gmail.com>
2025-06-07 18:50:54 -0500

    Diseño mensajes

--- src/components/chat/MessagesChat.jsx
+++ src/components/chat/MessagesChat.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 
 const MessagesChat = () => {
   return (
-    <div>
-      <h1>Mensajes</h1>
-    </div>
+    <main className="bg-indigo-100 p-4 space-y-2">
+      <article className='flex gap-x-2'>
+        <img className='rounded-full size-10' src="https://randomuser.me/api/portraits/thumb/men/67.jpg" alt="" />
+        <div className='bg-white rounded-md p-2 text-gray-700 max-w-[70%]'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, in sint officiis neque fuga dolores eum tempora. Nostrum blanditiis distinctio sunt delectus, minima eveniet ullam, veniam, quisquam totam vitae mollitia!
+          </p>
+          <p className='text-right text-xs '>right now</p>
+        </div>
+      </article>
+
+
+      <article className='flex gap-x-2 flex-row-reverse'>
+        <img className='rounded-full size-10' src="https://randomuser.me/api/portraits/thumb/men/67.jpg" alt="" />
+        <div className='bg-white rounded-md p-2 text-gray-700 max-w-[70%]'>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis, in sint officiis neque fuga dolores eum tempora. Nostrum blanditiis distinctio sunt delectus, minima eveniet ullam, veniam, quisquam totam vitae mollitia!
+          </p>
+          <p className='text-right text-xs '>right now</p>
+        </div>
+      </article>
+    </main>
+
   )
 }
 
